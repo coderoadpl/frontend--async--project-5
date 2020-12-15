5977ff46bc8ef32d7ea7d8e4ff84ce00adbe298b
Mateusz Choma <kontakt@coderoad.pl>
2020-12-15 08:05:23 +0100

    Fetching weather by clicking button

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
 const APPID = 'a798d078efa04620523b043cc93248e8'
-const query = 'Lublin,pl'
 
 class App {
 
     constructor() {
+        this.query = 'Lublin,pl'
+
         this.init()
     }
 
     init() {
-        fetchData(`https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${APPID}&units=metric`)
+        this.fetchWeather()
+    }
+
+    fetchWeather() {
+        return fetchData(`https://api.openweathermap.org/data/2.5/forecast?q=${this.query}&appid=${APPID}&units=metric`)
             .then(console.log)
     }
 
     render() {
         const container = document.createElement('div')
+
+        const button = new Button('Fetch weather', () => this.fetchWeather())
+
+        container.appendChild(button.render())
 
         return container
     }
