b511fac8b858f4a8838b910451885a696a6691d3
Mateusz Choma <kontakt@coderoad.pl>
2020-12-15 08:00:54 +0100

    Fetching weather data

--- App.js
+++ App.js
@@ -1,4 +1,21 @@
+const APPID = 'a798d078efa04620523b043cc93248e8'
+const query = 'Lublin,pl'
+
 class App {
 
+    constructor() {
+        this.init()
+    }
+
+    init() {
+        fetchData(`https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${APPID}&units=metric`)
+            .then(console.log)
+    }
+
+    render() {
+        const container = document.createElement('div')
+
+        return container
+    }
 
 }
